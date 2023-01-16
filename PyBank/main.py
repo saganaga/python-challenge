ac95e246c891a2baccf32478e6a04dd719a2da4a
saganaga <saganaga29@me.com>
2023-01-16 09:52:52 -0600

    Adds Average Change

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,7 @@ import csv
 # Variable declarations
 total_months = 0
 total = 0
+changes = list()
 
 # Set path for file
 csvpath = os.path.join(".", "Resources", "budget_data.csv")
@@ -17,13 +18,27 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
 
     row_count = 0
+
+    # prev_pl is previous row Profit/Losses value
+    prev_pl = None
+
     for row in csvreader:
 
         # set row counter
         row_count = row_count + 1
 
-        # assign total amount of Profits/Losses, convert to integer
-        total = total + int(row[1])
+        # current_pl is current row Profit/Losses value
+        current_pl = int(row[1])
+
+        # assign total amount of Profit/Losses, convert to integer
+        total = total + current_pl
+
+        if prev_pl is not None:
+            change = current_pl - prev_pl  
+            changes.append(change)
+
+        # store the current_pl so that in the next iteration in the loop it becomes the prev_pl
+        prev_pl = current_pl
 
     # assign total number of months in dataset
     total_months = row_count
@@ -32,3 +47,6 @@ print("Financial Analysis")
 print("-" * 28)
 print(f"Total Months: {row_count}")
 print("Total: $" + str(total))
+print(f"Average Change: ${round(sum(changes)/len(changes),2)}")
+print(f"Greatest Increase in Profits: (date TODO) (${max(changes)})")
+print(f"Greatest Decrease in Profits: (date TODO) (${min(changes)})")
--- README.md
+++ README.md
@@ -15,7 +15,7 @@ The Python script analyzes the financial records to calculate each of the follow
 
     [x] The net total amount of "Profit/Losses" over the entire period
 
-    [ ] The changes in "Profit/Losses" over the entire period, and then the average of those changes
+    [x] The changes in "Profit/Losses" over the entire period, and then the average of those changes
 
     [ ] The greatest increase in profits (date and amount) over the entire period
 
